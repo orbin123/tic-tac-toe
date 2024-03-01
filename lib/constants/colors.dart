d440471601f280783f83c16868bac6d32d57f6aa
Orbin <orbin@Orbins-MacBook-Air.local>
2024-03-01 15:03:30 +0530

    first commit

---
+++ lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class MainColor {
+  static Color primaryColor = const Color(0xFFff4b4b);
+  static Color accentColor = const Color(0xFF4169e8);
+  static Color secondaryColor = const Color(0xFFffca27);
+}
